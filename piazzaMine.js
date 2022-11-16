2ddf5d983dd9024ee418425b5197841f76a83bc0
Gabry <cregg27@gmail.com>
2022-11-16 09:22:10 +0100

    Primo commit

---
+++ piazzaMine.js
@@ -0,0 +1,5 @@
+let grid = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+function piazzaMine(){
+    let randomMina = Math.floor((Math.random() * 15))
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+let e = document.getElementById("abc");
+
+e.addEventListener('click', prova, false);
+let t = document.getElementById("test").innerHTML
+
+function prova(){
+    t = "Prova";
+}
